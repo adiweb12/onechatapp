69bf6142125b3c77dfd628677ac76a427442b82a
adiweb12 <vsadith051@gmail.com>
2026-02-14 06:51:45 +0530

    Add files via upload

--- frontend/lib/screens/group/create_group_screen.dart
+++ frontend/lib/screens/group/create_group_screen.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class CreateGroupScreen extends StatelessWidget {
+  const CreateGroupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Create Group')),
+      body: const Center(child: Text('Create Group Screen')),
+    );
+  }
+}
